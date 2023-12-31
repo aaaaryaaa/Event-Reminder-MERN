4267a2fcd67b29301d42ec5d6774069ded1b83d2
Aarya R <aarya.masters@gmail.com>
2023-12-31 14:32:21 +0530

    added controllers & backend is 90% done

--- backend/routes/eventRemRoutes.js
+++ backend/routes/eventRemRoutes.js
@@ -1,39 +1,23 @@
 //importing
 const express = require('express')
-const eventRemModel = require('../models/eventRemModel')
+const { createReminder, getAllReminders, getReminder, updateReminder, deleteReminder } = require('../controllers/eventRemController')
 
 //
 const router = express.Router()
 
 //get req for all displaying all event reminders
-router.get('/', (req,res)=>{
-    res.send({mssg: 'getting all'})
-})
+router.get('/', getAllReminders)
 
 //get ONE event reminder
-router.get('/:id', (req,res)=>{
-    res.send({mssg: 'getting ONE'})
-})
+router.get('/:id', getReminder)
 
 //post an event reminder
-router.post('/', async (req,res)=>{
-    const {title, desc, target, validity} = req.body
-    try{
-        const eventRem = await eventRemModel.create({title, desc, target, validity})
-        res.status(200).json(eventRem)
-    } catch(error){
-        res.status(400).json({error: error.message})
-    }
-})
+router.post('/', createReminder)
 
 //delete ONE event reminder
-router.delete('/:id', (req,res)=>{
-    res.send({mssg: 'deleting ONE'})
-})
+router.delete('/:id', deleteReminder)
 
 //updating ONE event reminder
-router.patch('/:id', (req,res)=>{
-    res.send({mssg: 'updating ONE'})
-})
+router.patch('/:id', updateReminder)
 
 module.exports=router
