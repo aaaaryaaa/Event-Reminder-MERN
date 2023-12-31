046b84be39437391b9e1639eb76cbfbde9cbbe8c
Aarya R <aarya.masters@gmail.com>
2023-12-31 20:04:02 +0530

    added daisy ui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ function App() {
   return (
     <div className="App">
       <h2 className="text-center text-blue-700">Hello world</h2>
+      <button className="btn btn-accent btn-outline">hi bro</button>
     </div>
   );
 }
